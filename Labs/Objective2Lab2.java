49b6838cf6a49237bc5803d0837223ec22f90074
SandraLeAnn <sharpnw21@gmail.com>
2022-09-21 13:35:24 -0700

    Remaining Objective 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Sandra Harpole";
+    int age = 27;
+    String state = "Idaho";
+
+  System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
