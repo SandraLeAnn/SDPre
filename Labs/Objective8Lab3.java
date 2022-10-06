4b56eb079c31d6e8291fa4541b8808a3aaf9c6ff
SandraLeAnn <sharpnw21@gmail.com>
2022-10-06 15:11:13 -0700

    O8O3

--- Labs/Objective8Lab3.java
+++ Labs/Objective8Lab3.java
@@ -2,15 +2,15 @@ public class Objective8Lab3 {
   public static void main(String[] args) {
     int counter = 1;
 
-    for (int i=1; i <= 20; i++) {
+    for (int i=1; i <= 20; i ++) {
       counter ++;
 
         if(counter % 2 == 0) {
-          System.out.println(  i + " is odd");
+          System.out.println(i + " is odd.");
         }
 
         else if(counter % 2 != 0){
-          System.out.println(i + " is even");
+          System.out.println(i + " is even.");
       }
     }
   }
